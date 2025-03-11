9e285493a22b1d0b26fe39ee3fcbb180fbe5b20b
Ricardo A. Sanjur G. <ricardosanjurg@gmail.com>
2025-03-10 21:58:31 -0500

    agregamos el mensaje a las personas para que se expresen y pusimos la mayoria de los campos opcionales y agregamos a aurora y paul

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -42,8 +42,6 @@ await prisma.persons.createMany({
       favoriteDessert: "Dulce de chocolate",
       favoriteColor: "Rosado y Verde",
       dreamAspiration: "Graduarme, ser millonaria, y tener un marido fiel",
-      favoriteSerieMovie: "",
-      favoriteMusicGenre: "",
       roleId: 3,
     },
     {
@@ -56,8 +54,15 @@ await prisma.persons.createMany({
       favoriteDessert: "Helado en galleta",
       favoriteColor: "Azul y Verde",
       dreamAspiration: "Trabajar para Microsoft o Apple",
-      favoriteSerieMovie: "",
-      favoriteMusicGenre: "",
+      message: `Vice Secretario del Partido Internacional Paulista
+
+Es un honor dirigirme a ustedes en esta ocasión tan especial. Mi nombre es Fredy Villarreal, estudiante de la Universidad Tecnológica de Panamá, sede Chiriquí. Nací el 10 de junio de 2002 y a lo largo de mi vida me he considerado una persona pacífica, siempre dispuesto a solucionar cualquier problema que surja dentro de nuestro partido.
+
+En mi tiempo libre, disfruto de la lectura, ver animes y series, así como de la música, que considero una de mis grandes pasiones. Amo enfrentar nuevos desafíos y aprender constantemente, porque el crecimiento personal y colectivo es clave para el éxito de cualquier organización.
+
+Ser parte del Partido Internacional Paulista es un privilegio que valoro enormemente. Compartir este camino con todos ustedes y trabajar juntos en la construcción de un futuro mejor es una experiencia única e inigualable. Espero que sigamos disfrutando de más momentos memorables y fortaleciendo nuestros lazos de unidad y compromiso.
+
+¡ARRIBA EL PIP, CARAJO!`,
       roleId: 2,
     },
     {
@@ -70,7 +75,32 @@ await prisma.persons.createMany({
       favoriteColor: "Azúl, Dorado.",
       favoriteDessert: "ni idea",
       favoriteMusicGenre: "todo",
+      favoriteVideoGame:
+        "ni idea; tengo muchos, tal vez, For Honor, Dance of Fire and Ice, Halo CE (más probable este), Age of Empires 2, Call of Duty 2004, Call of Duty 2, RDR2, Dead Cells, Dark Souls, SEKIRO (god), Left 4 Dead 2, War in the East (mentira que aún soy malo), muchos más ahí",
       roleId: 1,
+    },
+    {
+      fullName: "Aurora A. Acosta de León",
+      birthday: new Date("2006-04-21"),
+      email: "elly.oceans19@gmail.com",
+      alias: "Chelly",
+      hobby: "Leer, Ver k-dramas, Jugar en mi Psvita",
+      favoriteColor: "Morado",
+      dreamAspiration:
+        "Construir una vida lejos del ruido de la ciudad, en un hogar donde reine la paz y el amor. Formar una familia con un hombre fiel, alguien que valore cada risa, cada locura y cada detalle.",
+      message: `Soy Aurora A. Acosta de León, aunque muchos me conocen como Chelly o "tetona". Soy una mezcla de locura, dulzura y determinación. Amo perderme en los libros, emocionarme con los k-dramas y disfrutar de la adrenalina de los videojuegos en mi Psvita. El morado es mi color favorito, porque refleja mi esencia: intensa, profunda y llena de vida. Me encanta soñar en grande y vivir cada momento con pasión, siempre con una sonrisa en el rostro y una chispa de espontaneidad en el corazón. Mi mayor anhelo es construir una vida lejos del ruido de la ciudad, en un hogar donde reine la paz y el amor. Sueño con formar una familia con un hombre fiel, alguien que valore cada risa, cada locura y cada detalle. No quiero riquezas desbordantes, pero sí la estabilidad suficiente para vivir sin preocupaciones, disfrutando de cada instante con las personas que más amo. Sé que mi camino apenas comienza, pero tengo la certeza de que lograré cada una de mis metas con esfuerzo, fe y una dosis de mi inquebrantable determinación. Nací el 21 de abril del 2006 "cagada" pero nací, mi correo electrónico es elly.oceans19@gmail.com`,
+      roleId: 5,
+    },
+    {
+      fullName: "Paul Sánchez",
+      birthday: new Date("2004-11-24"),
+      email: "paulanibalsanchez24@gmail.com",
+      alias: "Paul, Awebao, Idiota",
+      hobby: "La guitarra, Skate, Jugar videojuegos, Películas",
+      favoriteFood: "La pasta",
+      favoriteColor: "Negro, rojo, blanco, gris",
+      dreamAspiration: "Ser un músico reconocido",
+      roleId: 6,
     },
   ],
 });
